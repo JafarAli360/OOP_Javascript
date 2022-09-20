4611422f93b0bacf506fa691a6c5291c542fffb7
JafarAli360 <ahmad.19058@mhs.unesa.ac.id>
2022-09-20 15:17:10 +0700

    first commit

---
+++ exercise4.js
@@ -0,0 +1,16 @@
+// class mobil {
+//   constructor(merek) {
+//     this.merek = merek;
+//   }
+// }
+// class model extends mobil {
+//   constructor(jenis, merek) {
+//     super(merek);
+//     this.jenis = jenis;
+//   }
+//   introduce() {
+//     console.log(`saya memiliki ${this.merek}, yang berjenis ${this.jenis}`);
+//   }
+// }
+// let Yamaha = new model("Avanza", "Xenia");
+// Yamaha.introduce();
